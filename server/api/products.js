f274d1d3d8ca279e576acbe116c2c86bf3c3c897
Christian Anderson <somecallmehans@gmail.com>
2021-11-09 16:48:52 -0500

    Feature: Single Product View (bare bones)

--- server/api/products.js
+++ server/api/products.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { models: { Liquor }} = require('../db')
 
+//get all the liquors
 router.get('/', async (req, res, next) => {
   try {
     const products = await Liquor.findAll();
@@ -10,3 +11,14 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+//get a single liquor based on id
+router.get('/:liquorId', async (req, res, next) => {
+  try {
+    const liquor = await Liquor.findByPk(req.params.liquorId);
+    res.send(liquor);
+  } catch (error) {
+    next(error);
+  }
+})
+
+module.exports = router;
