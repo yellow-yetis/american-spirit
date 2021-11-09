b9888112d8b7cd60f2b38f278d76fbbc87dca526
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-09 16:02:21 -0500

    created models

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,37 @@
 //this is the access point for all things database related!
 
 const db = require('./db')
-
 const User = require('./models/User')
-
+const Liquor = require('./models/Liquor')
+const Cart = require('./models/Cart')
+const Sequelize = require('sequelize')
 //associations could go here!
+
+const cartLiquor = db.define('cartLiquor', {
+  cartQuantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  cartPrice: {
+    type: Sequelize.FLOAT
+  }
+})
+
+Cart.belongsTo(User)
+User.hasOne(Cart)
+
+Cart.belongsToMany(Liquor,{through: cartLiquor})
+Liquor.belongsToMany(Cart,{through: cartLiquor})
+
+
+
 
 module.exports = {
   db,
   models: {
     User,
+    Cart,
+    Liquor,
+    cartLiquor
   },
 }
---
+++ server/db/models/Liquor.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Liquor = db.define('liquor', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  ABV: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+
+  stock: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
+  },
+})
+
+module.exports = Liquor
