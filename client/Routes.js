ff2752c0c58b721a2c5a67eb6c7039171bc53c9b
Christian Anderson <somecallmehans@gmail.com>
2021-11-09 17:36:02 -0500

    Updated nav + got rid of some boilerplate text

--- client/Routes.js
+++ client/Routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
+import AllProducts from './components/AllProducts';
 import {me} from './store'
 
 /**
@@ -21,13 +22,14 @@ class Routes extends Component {
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
-            <Redirect to="/home" />
+            <Route exact path="/products" component={AllProducts} />
           </Switch>
         ) : (
           <Switch>
             <Route path='/' exact component={ Login } />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route exact path="/products" component={AllProducts} />
           </Switch>
         )}
       </div>
