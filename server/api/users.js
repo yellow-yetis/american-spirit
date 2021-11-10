f85aabfd1213339332acab3360c49d4ac8fd434d
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-11-10 16:13:34 -0500

    added gatekeepingmiddleware to allow only admin to access all users

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,24 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
-
-router.get('/', async (req, res, next) => {
+const router = require('express').Router();
+const {
+  models: { User },
+} = require('../db');
+const { requireToken } = require('./gatekeepingmiddleware');
+module.exports = router;
+requireToken;
+router.get('/', requireToken, async (req, res, next) => {
   try {
+    console.log('This is what I want ', req.user);
+    if (!req.user.isAdmin) {
+      return res.status(403).send('Access Denied');
+    }
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ['id', 'username'],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
