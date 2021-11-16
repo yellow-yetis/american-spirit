5eda96bcc91f5cd4a0c2d837143ee6134acabcb2
Devarat Patel <devaratpatel1@gmail.com>
2021-11-16 12:35:36 -0500

    updated api index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,18 +3,9 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/products', require('./products'));
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-router.use('/categories', require('./categories'));
->>>>>>> main
-
 router.use('/cart', require('./cart'));
-=======
 router.use('/categories', require('./categories'));
 router.use('/order', require('/orders'));
->>>>>>> Stashed changes
-
 router.use((req, res, next) => {
   const error = new Error('Not Found');
   error.status = 404;
