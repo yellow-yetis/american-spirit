e706a855e494321b1b4c61f8dc1f662b6c2f7908
Christian Anderson <somecallmehans@gmail.com>
2021-11-17 10:29:01 -0500

    Updated error handling front end cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -9,7 +9,7 @@ export class Cart extends Component {
   constructor() {
     super();
     this.state = {
-      productArr: [],
+      error: '',
       show: false,
     };
     this.handleChange = this.handleChange.bind(this);
@@ -29,15 +29,28 @@ export class Cart extends Component {
   }
 
   async handleChange(e, product){
-    let itemUpdatedInCart = {
-      ...product,
-      cartLiquor: {
-        liquorQuantity: e.target.value,
-        liquorTotalPrice: e.target.value * product.price,
+    if(e.target.value <= product.stock && e.target.value > 0){
+      let itemUpdatedInCart = {
+        ...product,
+        cartLiquor: {
+          liquorQuantity: e.target.value,
+          liquorTotalPrice: e.target.value * product.price,
+        }
       }
+      this.setState({
+        error: ''
+      })
+      await this.props.updateCart(this.props.userId, itemUpdatedInCart);
+      await this.props.fetchCartProducts(this.props.userId);
+    } else if (e.target.value <= 0){
+      this.setState({
+        error: 'Only quantities 1 or greater are allowed'
+      })
+    } else if (e.target.value > product.stock){
+      this.setState({
+        error: 'We do not have that many products in stock, please lower quantity'
+      })
     }
-    await this.props.updateCart(this.props.userId, itemUpdatedInCart);
-    await this.props.fetchCartProducts(this.props.userId);
   }
 
   async removeItem(userId, productId){
@@ -52,7 +65,6 @@ export class Cart extends Component {
   }
 
   render() {
-
       return (
         <div>
           <h1 className="center">Shopping Cart</h1>
